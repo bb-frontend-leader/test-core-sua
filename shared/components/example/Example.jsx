4e78ea360ba2fa1fc4b3434400b3935b274341c6
bb_coor_frontend <coor_frontend@booksandbooksdigital.com.co>
2022-02-11 08:15:45 -0500

    Initial commit

---
+++ shared/components/example/Example.jsx
@@ -0,0 +1,12 @@
+import css from "../../styles/index.css";
+
+const Example = () => {
+    return (
+    <>
+        <h1 className={`${css['body']}`} >Example</h1>
+        <h2 className={`${css['body']}`} >Example</h2>
+    </>
+    );
+};
+
+export { Example }
